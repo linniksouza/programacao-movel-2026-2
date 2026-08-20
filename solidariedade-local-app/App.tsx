6c89cfccb9484dd8a98e4c19330e2c1421cb04bc
linniksouza <linniksouza@bemol.com.br>
2026-08-20 19:35:17 -0400

    Criacao do projeto

---
+++ solidariedade-local-app/App.tsx
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Seja bem vindo ao Solidariedade Local</Text>
+      <Button title="Tela inicial" />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
