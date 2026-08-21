28e1ccb3939a9df2bdb006c9f30f018f920635d0
linniksouza <linniksouza@bemol.com.br>
2026-08-21 00:47:38 -0400

    Corrigindo teste para a pipeline funcionar

--- solidariedade-local-app/src/__test__/App.test.tsx
+++ solidariedade-local-app/src/__test__/App.test.tsx
@@ -5,7 +5,7 @@ import App from "../App";
 describe("Validando o componente React 'App'", () => {
     it("Validando mensagem de boas vindas", async () => {
         const { findByText } = await render(<App />);
-        const temMgsBoasVindas = await findByText(/Seja bem vindo ao Solidariedade Local!/i);
+        const temMgsBoasVindas = await findByText(/Seja bem vindo ao Solidariedade Local/i);
 
         expect(temMgsBoasVindas).toBeTruthy();
     });
